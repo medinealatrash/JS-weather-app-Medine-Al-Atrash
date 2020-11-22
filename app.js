aea5f4a33b70947c3861fff1001a8e47ed1fc21c
Medine <you@example.com>
2020-11-22 17:51:23 +0100

    select the elements from the HTML document

---
+++ app.js
@@ -0,0 +1,13 @@
+//select the necessary elements from the HTML document
+let container = document.querySelector('.container');
+let containerTop = document.querySelector('.container-top');
+let messageAccepted = document.querySelector('.accepted');
+let messageError = document.querySelector('.error');
+let errorFetch = document.querySelector('.errorFetch')
+let inputForm = document.querySelector('.input-form');
+let containerCenter = document.querySelector('.container-center');
+let difference = document.querySelector('.difference');
+let containerBottom = document.querySelector('.container-bottom');
+let loadingImg = document.querySelector('.loading') 
+let input = document.querySelector('.my-input');
+let btn = document.querySelector('.my-btn');
